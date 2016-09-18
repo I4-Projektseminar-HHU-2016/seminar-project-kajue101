0e1e8baaad139a7b8d601303ce8f4c7a913acf20
kajue101 <karoline.juettner@hhu.de>
2016-09-19 00:37:52 +0200

    Probleme mit Datei behoben, Berechnung der Installationen pro Sprache vollständig

--- Projekt.py
+++ Projekt.py
@@ -1,6 +1,9 @@
 import csv
 
+
 #Funktionen
+
+#Funktion zur Berechnung der Top20
 def top20(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 	top20liste = []
 	i = 0
@@ -10,7 +13,8 @@ def top20(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 		liste1[liste1.index(max(liste1))] = 0 #der Maximalwert von Liste 1 wird auf 0 gesetzt, damit im nächsten Durchlauf der nächsthöchste Wert betrachtet werden kann
 		i = i+1
 	return top20liste
-	
+
+#Funktion zur Berechnung der Flop20
 def flop20(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 	flop20liste = []
 	i = 0
@@ -20,8 +24,21 @@ def flop20(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 		liste1[liste1.index(min(liste1))] = 10000000 #der Maximalwert von Liste 1 wird auf 10000000 (beliebiger hoher Wert) gesetzt, damit im nächsten Durchlauf der nächstkleinste Wert betrachtet werden kann
 		i = i+1
 	return flop20liste
+	
+#Funktion zur Berechnung der Flop40
+def flop40(liste1, liste2): #Liste1 und Liste2 werden eingelesen
+	flop40liste = []
+	i = 0
+	while i < 40:  #weil nur die letzten 20 betrachtet werden
+		flop40liste.append(min(liste1)) #die neue Liste bekommt den kleinsten Wert von Liste 1 zugeordnet
+		flop40liste.append(liste2[liste1.index(min(liste1))]) #die neue Liste bekommt den Eintrag von Liste 2 zugeordnet, der den selben Index besitzt wie das Minimum von Liste 1
+		liste1[liste1.index(min(liste1))] = 10000000 #der Maximalwert von Liste 1 wird auf 10000000 (beliebiger hoher Wert) gesetzt, damit im nächsten Durchlauf der nächstkleinste Wert betrachtet werden kann
+		i = i+1
+	return flop40liste
 
+	
 #Listen erstellen 
+
 #Listen für die gesamte CSV-Datei
 version_crashes_2015 = []
 country_installs_2015 = []
@@ -32,6 +49,7 @@ language_installs_2015 = []
 country_installs_2016 = []
 overview_2016 = []
 language_installs_2016 = []
+
 #Listen für die benötigten Spalten
 total_user_installs_2015 = []
 total_user_installs_2015_top = []
@@ -61,11 +79,8 @@ total_user_installs_2016_languages_top_2 = []
 total_user_installs_2016_languages_flop_2 = []
 
 
-
-
-
-
 #Einlesen der CSV-Dateien im Ordner Google Play 2015
+
 with open ('Google Play 2015/com-ichi2-anki-app-version-crashes.csv', 'rU') as csv_input:
 	reader = csv.reader(csv_input, delimiter=';')
 	for row in reader:
@@ -96,6 +111,7 @@ with open ('Google Play 2015/com-ichi2-anki-language-installs.csv', 'rU') as csv
 	for row in reader:
 		language_installs_2015.append(row)
 		
+		
 #Einlesen der CSV-Dateien im Ordner Google Play 2016
 
 with open ('Google Play 2016/installs_com.ichi2.anki_201603_country.csv', 'rU') as csv_input:
@@ -108,16 +124,17 @@ with open ('Google Play 2016/installs_com.ichi2.anki_201603_overview.csv', 'rU')
 	for row in reader:
 		overview_2016.append(row)
 		
-#with open ('Google Play 2016/installs_com.ichi2.anki_201603_language.csv', 'rU') as csv_input:
-#	reader = csv.reader(csv_input, delimiter='	')
-#	for row in reader:
-#		language_installs_2016.append(row)
+with open ('Google Play 2016/installs_com.ichi2.anki_201603_language.csv', 'rU') as csv_input:
+	reader = csv.reader(csv_input, delimiter=';')
+	for row in reader:
+		language_installs_2016.append(row)
 
+		
 #Voranstehende, bedeutungslose Listenelemente löschen
 del country_installs_2015[0:5]
 del country_installs_2016[0] 
 del language_installs_2015[0:5]
-#del language_installs_2016[0]
+del language_installs_2016[0:3]
 
 
 #Listen mit einzelnen Spalten befüllen
@@ -146,7 +163,7 @@ for eintrag in language_installs_2016:
 	total_user_installs_2016_languages_flop.append(eintrag[8])
 
 	
-#String in int umwandeln
+#String in Integer umwandeln
 for string in total_user_installs_2015:
 	total_user_installs_2015_2.append(int(string))
 	total_user_installs_2015_top_2.append(int(string))
@@ -172,13 +189,14 @@ flop20installs_country_2015 = flop20(total_user_installs_2015_flop_2, laenderabk
 flop20installs_country_2016 = flop20(total_user_installs_2016_flop_2, laenderabkuerzungen_2016)
 
 top20installs_language_2015 = top20(total_user_installs_2015_languages_top_2, languages_2015)
-flop20installs_language_2015 = flop20(total_user_installs_2015_languages_flop_2, languages_2015)
-#top20installs_language_2016 = top20(total_user_installs_2016_languages_top_2, languages_2016)
-#flop20installs_language_2016 = flop20(total_user_installs_2016_languages_flop_2, languages_2016)
+flop40installs_language_2015 = flop40(total_user_installs_2015_languages_flop_2, languages_2015)
+top20installs_language_2016 = top20(total_user_installs_2016_languages_top_2, languages_2016)
+flop40installs_language_2016 = flop40(total_user_installs_2016_languages_flop_2, languages_2016)
 
-print (total_user_installs_2015_languages_flop_2)
+print (language_installs_2016[0])
 		
 
 #Diagramme
 
 #TODO: D3 Diagramm mit den Informationen aus country_installs_2015 und country_installs_2016
+#TODO: Histogramm aus language_installs_2015 und language_installs_2016
