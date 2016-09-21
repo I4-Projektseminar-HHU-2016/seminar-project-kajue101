2b87431292e86d24e71c3852dca0dbc9d1453e74
kajue101 <karoline.juettner@hhu.de>
2016-09-21 02:48:06 +0200

    some comments and some style changes added

--- Projekt.py
+++ Projekt.py
@@ -12,7 +12,7 @@ def top20(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 		top20liste.append(liste2[liste1.index(max(liste1))]) #die neue Liste bekommt den Eintrag von Liste 2 zugeordnet, der den selben Index besitzt wie das Maximum von Liste 1
 		liste1[liste1.index(max(liste1))] = 0 #der Maximalwert von Liste 1 wird auf 0 gesetzt, damit im nächsten Durchlauf der nächsthöchste Wert betrachtet werden kann
 		i = i+1
-	return top20liste
+	return top20liste #Die neue Liste wird zurückgegeben
 
 #Funktion zur Berechnung der Flop20
 def flop20(liste1, liste2): #Liste1 und Liste2 werden eingelesen
@@ -23,37 +23,40 @@ def flop20(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 		flop20liste.append(liste2[liste1.index(min(liste1))]) #die neue Liste bekommt den Eintrag von Liste 2 zugeordnet, der den selben Index besitzt wie das Minimum von Liste 1
 		liste1[liste1.index(min(liste1))] = 10000000 #der Maximalwert von Liste 1 wird auf 10000000 (beliebiger hoher Wert) gesetzt, damit im nächsten Durchlauf der nächstkleinste Wert betrachtet werden kann
 		i = i+1
-	return flop20liste
+	return flop20liste #Die neue Liste wird zurückgegeben
 	
 #Funktion zur Berechnung der Flop40
 def flop40(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 	flop40liste = []
 	i = 0
-	while i < 40:  #weil nur die letzten 20 betrachtet werden
+	while i < 40:  #weil nur die letzten 40 betrachtet werden
 		flop40liste.append(min(liste1)) #die neue Liste bekommt den kleinsten Wert von Liste 1 zugeordnet
 		flop40liste.append(liste2[liste1.index(min(liste1))]) #die neue Liste bekommt den Eintrag von Liste 2 zugeordnet, der den selben Index besitzt wie das Minimum von Liste 1
 		liste1[liste1.index(min(liste1))] = 10000000 #der Maximalwert von Liste 1 wird auf 10000000 (beliebiger hoher Wert) gesetzt, damit im nächsten Durchlauf der nächstkleinste Wert betrachtet werden kann
 		i = i+1
-	return flop40liste
+	return flop40liste #Die neue Liste wird zurückgegeben
 	
+#Diese Funktion sortiert eine Liste aufsteigend (mit dazugehörigem Wert aus der anderen Liste)
 def aufsteigend(liste1, liste2): #Liste1 und Liste2 werden eingelesen
 	aufsteigendeliste = []
 	i = 0
 	k = len(liste1)
-	while i < k:  #weil nur die letzten 20 betrachtet werden
+	while i < k:  #die ganze Liste wird abgearbeitet
 		aufsteigendeliste.append(min(liste1)) #die neue Liste bekommt den kleinsten Wert von Liste 1 zugeordnet
 		aufsteigendeliste.append(liste2[liste1.index(min(liste1))]) #die neue Liste bekommt den Eintrag von Liste 2 zugeordnet, der den selben Index besitzt wie das Minimum von Liste 1
-		del(liste2[liste1.index(min(liste1))])
-		del(liste1[liste1.index(min(liste1))])
+		del(liste2[liste1.index(min(liste1))]) #Der Eintrag aus Liste 1 wird gelöscht
+		del(liste1[liste1.index(min(liste1))]) #Der Eintrag aus Liste 2 wird gelöscht
 		i = i+1
-	return aufsteigendeliste
+	return aufsteigendeliste #Die neue Liste wird zurückgegeben
 	
+#Funktion zur Berechnung der Summe einer Liste
 def summe(liste):
 	summe = 0
 	for i in liste:
-		summe += i
+		summe += i #jedes neue Element wird zu "summe" hinzuaddiert
 	return summe
 	
+#Funktion zur Berechnung der Differenz in Prozent
 def differenz_in_prozent(wert1,wert2):
 	float(wert1)
 	float(wert2)
@@ -61,6 +64,7 @@ def differenz_in_prozent(wert1,wert2):
 	diff = diff*100
 	return diff
 
+#Diese Funktion trennt die zusammengefügten Listen wieder (Index gerade)
 def splitlist1(liste):
 	newlist1=[]
 	i = 0
@@ -69,7 +73,8 @@ def splitlist1(liste):
 		newlist1.append(liste[i])
 		i+=2
 	return newlist1
-	
+
+#Diese Funktion trennt die zusammengefügten Listen wieder (Index ungerade)
 def splitlist2(liste):
 	newlist2=[]
 	i = 1
@@ -80,8 +85,6 @@ def splitlist2(liste):
 	return newlist2
 	
 
-	
-	
 #Listen erstellen 
 
 #Listen für die gesamte CSV-Datei
@@ -92,7 +95,6 @@ country_ratings_2015 = []
 device_crashes_2015 = []
 language_installs_2015 = []
 country_installs_2016 = []
-overview_2016 = []
 language_installs_2016 = []
 country_installs_complete_2016 = []
 
@@ -157,6 +159,9 @@ daily_installs_2016_complete = []
 daily_installs_2016_complete_2 = []
 daily_uninstalls_2016_complete = []
 daily_uninstalls_2016_complete_2 = []
+laenderabkuerzungen_2015_klein = []
+laenderabkuerzungen_2016_klein = []
+laenderabkuerzungen_2015_ratings_klein = []
 
 
 #Einlesen der CSV-Dateien im Ordner Google Play 2015
@@ -179,7 +184,7 @@ with open ('Google Play 2015/com-ichi2-anki-country-installs-complete.csv', 'rU'
 with open ('Google Play 2015/com-ichi2-anki-country-ratings.csv', 'rU') as csv_input:
 	reader = csv.reader(csv_input, delimiter=';')
 	for row in reader:
-		country_ratings_2015.append(row) #Bewertungen sind kaputt
+		country_ratings_2015.append(row)
 		
 with open ('Google Play 2015/com-ichi2-anki-device-crashes.csv', 'rU') as csv_input:
 	reader = csv.reader(csv_input, delimiter=';')
@@ -198,11 +203,6 @@ with open ('Google Play 2016/installs_com.ichi2.anki_201603_country.csv', 'rU') 
 	reader = csv.reader(csv_input, delimiter=';')
 	for row in reader:
 		country_installs_2016.append(row)
-		
-with open ('Google Play 2016/installs_com.ichi2.anki_201603_overview.csv', 'rU') as csv_input:
-	reader = csv.reader(csv_input, delimiter=';')
-	for row in reader:
-		overview_2016.append(row)
 		
 with open ('Google Play 2016/installs_com.ichi2.anki_201603_language.csv', 'rU') as csv_input:
 	reader = csv.reader(csv_input, delimiter=';')
@@ -282,7 +282,7 @@ for eintrag in country_installs_complete_2016:
 	daily_uninstalls_2016_complete.append(eintrag[10])
 
 	
-#String in Integer umwandeln
+#Strings in Integers umwandeln
 for string in total_user_installs_2015:
 	total_user_installs_2015_2.append(int(string))
 	total_user_installs_2015_top_2.append(int(string))
@@ -344,58 +344,69 @@ for string in daily_uninstalls_2016_complete:
 	daily_uninstalls_2016_complete_2.append(int(string))
 
 
-
-
 #Berechnungen
-	
+
+#Top20 und Flop20 der Installationen pro Land
 top20installs_country_2015 = top20(total_user_installs_2015_top_2, laenderabkuerzungen_2015)
 top20installs_country_2016 = top20(total_user_installs_2016_top_2, laenderabkuerzungen_2016)
 flop20installs_country_2015 = flop20(total_user_installs_2015_flop_2, laenderabkuerzungen_2015)
 flop20installs_country_2016 = flop20(total_user_installs_2016_flop_2, laenderabkuerzungen_2016)
 
+#Top20 und Flop40 der Installationen pro Sprache
 top20installs_language_2015 = top20(total_user_installs_2015_languages_top_2, languages_2015)
 flop40installs_language_2015 = flop40(total_user_installs_2015_languages_flop_2, languages_2015)
 top20installs_language_2016 = top20(total_user_installs_2016_languages_top_2, languages_2016)
 flop40installs_language_2016 = flop40(total_user_installs_2016_languages_flop_2, languages_2016)
 
+#Top20 und Flop40 der Abstürze pro Gerät
 top20crashes_2015 = top20(crashes_2015_top_2, devices)
 flop40crashes_2015 = flop40(crashes_2015_flop_2, devices)
 
+#Liste der Versionen aufsteigend sortiert, mit dazugehörigem Gerät
 versionen_crashes_aufsteigend = aufsteigend(version_2, crashes_2015_version_2)
 
+#Summe der Installationen 2015
 summe_installs_2015 = summe(daily_installs_2015_2)
 
+#Summe der Installationen 2016
 summe_installs_2016 = summe(daily_installs_2016_2)
 
+#Summe der Abstürze 2015
 summe_crashes_2015 = summe(crashes_2015_2)
 
+#Berechnung des Prozentsatzes an Nutzern, die die App behalten haben (2015)
 summe_current_user_installs_2015 = summe(current_user_installs_2015_2)
 summe_total_user_installs_2015 = summe(total_user_installs_2015_complete_2)
 behalten_quote_2015 = differenz_in_prozent(summe_current_user_installs_2015, summe_total_user_installs_2015)
 
+#Berechnung des Prozentsatzes an Nutzern, die die App behalten haben (2016)
 summe_current_user_installs_2016 = summe(current_user_installs_2016_2)
 summe_total_user_installs_2016 = summe(total_user_installs_2016_complete_2)
 behalten_quote_2016 = differenz_in_prozent(summe_current_user_installs_2016, summe_total_user_installs_2016)
 
+#Berechnung des Prozentsatzes an Nutzern, die die App behalten haben (Gesamt)
 summe_current_user_installs_gesamt = summe_current_user_installs_2015+summe_current_user_installs_2016
 summe_total_user_installs_gesamt = summe_total_user_installs_2015+summe_total_user_installs_2016
 behalten_quote_gesamt = differenz_in_prozent(summe_current_user_installs_gesamt, summe_total_user_installs_gesamt)
 
+#Deinstallationsquoten (2015, 2016, Gesamt)
 deinstallationsquote_2015 = 100 - behalten_quote_2015
 deinstallationsquote_2016 = 100 - behalten_quote_2016
 deinstallationsquote_gesamt = 100 - behalten_quote_gesamt
 
+#Summen der täglichen Installationen und Deinstallationen (2015 & 2016)
 summe_daily_installs_2015 = summe(daily_installs_2015_complete_2)
 summe_daily_uninstalls_2015 = summe(daily_uninstalls_2015_complete_2)
 summe_daily_installs_2016 = summe(daily_installs_2016_complete_2)
 summe_daily_uninstalls_2016 = summe(daily_uninstalls_2016_complete_2)
 
+#Differenz zwischen Installationen und Deinstallationen (2015 & 2016)
 differenz_installs_uninstalls_2015 = summe_daily_installs_2015 - summe_daily_uninstalls_2015
 differenz_installs_uninstalls_2016 = summe_daily_installs_2016 - summe_daily_uninstalls_2016
 
+#Splitten einiger zusammengesetzer Listen, für die Diagramme
 version_list = splitlist1(versionen_crashes_aufsteigend)
 crashes_list = splitlist2(versionen_crashes_aufsteigend)
-
 crashes_list_2 = splitlist1(top20crashes_2015)
 devices_list = splitlist2(top20crashes_2015)
 crashes_list_flop = splitlist1(flop40crashes_2015)
@@ -409,10 +420,7 @@ installs_list_3 = splitlist1(flop40installs_language_2015)
 language_list_4 = splitlist2(flop40installs_language_2016)
 installs_list_4 = splitlist1(flop40installs_language_2016)
 
-laenderabkuerzungen_2015_klein = []
-laenderabkuerzungen_2016_klein = []
-laenderabkuerzungen_2015_ratings_klein = []
-
+#Länderabkürzungen in Kleinbuchstaben umwandeln, für Weltkarte
 for eintrag in laenderabkuerzungen_2015:
 	laenderabkuerzungen_2015_klein.append(eintrag.lower())
 for eintrag in laenderabkuerzungen_2016:
@@ -420,11 +428,11 @@ for eintrag in laenderabkuerzungen_2016:
 for eintrag in laenderabkuerzungen_2015_ratings:
 	laenderabkuerzungen_2015_ratings_klein.append(eintrag.lower())
 
-
+#Dictionaries erstellen, die von den Weltdiagrammen eingelesen werden können
 country_dictionary_2015 = dict(zip(laenderabkuerzungen_2015_klein, total_user_installs_2015_2))
 country_dictionary_2016 = dict(zip(laenderabkuerzungen_2016_klein, total_user_installs_2016_2))
 ratings_dictionary_2015 = dict(zip(laenderabkuerzungen_2015_ratings_klein, ratings_2015_2))
-print (ratings_dictionary_2015)
+
 
 #Diagramme
 
@@ -533,7 +541,9 @@ world_style = Style(
   foreground='black',
   foreground_strong='black',
   foreground_subtle='#0B6121',
-  font_family='arial',
+  legend_font_size=20,
+  title_font_size=20,
+  legend_box_size=20,
   opacity='.6',
   opacity_hover='.9',
   transition=None,
